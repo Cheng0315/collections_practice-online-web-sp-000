c20679d0d9276564303ca54cac08a3eab4eea2d8
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 07:09:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
 
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(arr)
+
+end
+
+def swap_elements(arr)
+
+end
